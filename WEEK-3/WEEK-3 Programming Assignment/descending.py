c95eface3568915700a96c7b9124907a3bb590fe
ManishRavi <46834426+ManishRavi@users.noreply.github.com>
2019-02-13 21:43:55 +0530

    Create descending.py

---
+++ WEEK-3/WEEK-3 Programming Assignment/descending.py
@@ -0,0 +1,27 @@
+QUESTION:
+
+Define a Python function descending(l) that returns True if each element in its input list is at most as big as the one before it. 
+For instance:
+
+  >>> descending([])
+  True
+
+  >>> descending([4,4,3])
+  True
+
+  >>> descending([19,17,18,7])
+  False
+  
+  PROGRAM/SOLUTION
+  
+  def descending(l):
+    val = False
+    if l == []:
+        return True
+    for i in range(len(l) - 1):
+        if l[i] >= l[i + 1]:
+            val = True
+        else:
+            val = False
+            break
+    return val
